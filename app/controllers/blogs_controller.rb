c4f6d4622346a57e24307157ec412f90ebaf005b
okushogg <okusho721@gmail.com>
2020-11-04 07:41:24 +0000

    [Start] 3-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,19 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
   end
 
   def new
+    @blog = Blog.new
   end
   
   def create
     @blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_
+    @blog.save
+    redirect_to blogs_path
   end
 
   def edit
