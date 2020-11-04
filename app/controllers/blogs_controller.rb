64c6d2fdcfde63ee80b3ea2bf8664b6117e08040
okushogg <okusho721@gmail.com>
2020-11-04 06:46:36 +0000

    [Finsh] 3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,19 @@ class BlogsController < ApplicationController
 
   def new
   end
+  
+  def create
+    @blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_
+  end
 
   def edit
   end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
